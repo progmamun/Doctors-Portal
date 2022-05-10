f07c3d94b8af3c1861fa205b82764eca2bb82f73
progmamun <mamun333089@gmail.com>
2022-05-10 23:38:01 +0600

    contact

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -13,6 +14,7 @@ const Home = () => {
       <Services></Services>
       <MakeAppointment></MakeAppointment>
       <Testimonials></Testimonials>
+      <Contact></Contact>
     </div>
   );
 };
