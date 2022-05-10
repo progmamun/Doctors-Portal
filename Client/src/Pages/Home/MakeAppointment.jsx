3d37bd7464408fe62cbb51966d2fe1b0753729c4
progmamun <mamun333089@gmail.com>
2022-05-10 23:36:03 +0600

    testimonaials

--- Client/src/Pages/Home/MakeAppointment.jsx
+++ Client/src/Pages/Home/MakeAppointment.jsx
@@ -10,19 +10,20 @@ const MakeAppointment = () => {
       }}
       className="flex justify-center items-center"
     >
-      <div className="flex-1">
+      <div className="flex-1 hidden lg:block">
         <img className="mt-[-100px]" src={doctor} alt="" />
       </div>
-      <div className="flex-1">
-        <h3 className="text-xl text-primary">Appointment</h3>
-        <h2 className="text-3xl">Make an Appointment Today</h2>
-        <p className="text-white">
+      <div className="flex-1 px-5">
+        <h3 className="text-xl text-primary font-bold">Appointment</h3>
+        <h2 className="text-3xl text-white py-5">Make an Appointment Today</h2>
+        <p className="text-white pb-5">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero,
           pariatur voluptatum velit reprehenderit amet provident rerum deserunt
           facere veniam ea tempora possimus aliquam odit aliquid iste
           accusantium porro delectus eos animi alias nobis culpa praesentium
           minus. Ab quaerat labore delectus.
         </p>
+        <button className="primaryBtn">Get Started</button>
       </div>
     </section>
   );
