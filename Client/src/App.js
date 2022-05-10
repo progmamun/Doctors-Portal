389b0e58f38b2bb9b36bec249e65971211f1001f
progmamun <mamun333089@gmail.com>
2022-05-10 12:24:47 +0600

    nav

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        {/*  <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+      'dark',
+      'cupcake',
+    ],
   },
   plugins: [require('daisyui')],
 };
