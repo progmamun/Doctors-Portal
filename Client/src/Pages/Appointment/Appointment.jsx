6e0730d406a9c7e2f52156c03d8389cc641ff66b
progmamun <mamun333089@gmail.com>
2022-05-11 18:28:44 +0600

    available-appointment

--- Client/src/Pages/Appointment/Appointment.jsx
+++ Client/src/Pages/Appointment/Appointment.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppointmentBanner from './AppointmentBanner';
+import AvailableAppointments from './AvailableAppointments';
 
 const Appointment = () => {
   return (
     <div>
       <AppointmentBanner />
+      <AvailableAppointments />
     </div>
   );
 };
--- Client/src/Pages/Appointment/AppointmentBanner.jsx
+++ Client/src/Pages/Appointment/AppointmentBanner.jsx
@@ -3,16 +3,23 @@ import React, { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import chair from '../../assets/images/chair.png';
+import bg from '../../assets/images/bg.png';
 
-const AppointmentBanner = () => {
-  const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({ date, setDate }) => {
+  // const [date, setDate] = useState(new Date());
   return (
-    <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="" />
+    <div
+      style={{
+        background: `url(${bg})`,
+        backgroundSize: 'cover',
+      }}
+      className="hero min-h-screen"
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
         <div>
           <DayPicker mode="single" selected={date} onSelect={setDate} />
-          <p>You have Selected: {format(date, 'pp')}</p>
+          {/* <p>You have Selected: {format(date, 'pp')}</p> */}
         </div>
       </div>
     </div>
