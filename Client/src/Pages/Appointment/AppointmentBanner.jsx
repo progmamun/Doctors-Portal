2c9f91b0a165fd1dbd84a5162348cd10a229ff43
progmamun <mamun333089@gmail.com>
2022-05-11 17:52:57 +0600

    appointment-date-selecte

--- Client/src/Pages/Appointment/AppointmentBanner.jsx
+++ Client/src/Pages/Appointment/AppointmentBanner.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 import chair from '../../assets/images/chair.png';
 
 const AppointmentBanner = () => {
+  const [date, setDate] = useState(new Date());
   return (
     <div class="hero min-h-screen">
       <div class="hero-content flex-col lg:flex-row-reverse">
         <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="" />
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button class="btn btn-primary">Get Started</button>
+          <DayPicker mode="single" selected={date} onSelect={setDate} />
+          <p>You have Selected: {format(date, 'pp')}</p>
         </div>
       </div>
     </div>
