10f7c10578b4ddbb268a5bb920dc26a45e164c55
progmamun <mamun333089@gmail.com>
2022-05-11 20:53:56 +0600

    booking-modal

--- Client/src/Pages/Appointment/BookingModal.jsx
+++ Client/src/Pages/Appointment/BookingModal.jsx
@@ -1,30 +1,73 @@
 import React from 'react';
+import { format } from 'date-fns';
 
-const BookingModal = ({ treatment }) => {
-  const [name, slots] = treatment;
+const BookingModal = ({ date, treatment, setTreatment }) => {
+  const [_id, name, slots] = treatment;
+
+  const handleBooking = event => {
+    event.preventDefault();
+    const slot = event.target.slot.value;
+    console.log(_id, name, slot);
+    setTreatment(null);
+  };
   return (
     <div>
-      <input type="checkbox" id="booking-modal" className="modal-toggle" />
-      <div className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
+      <input type="checkbox" id="booking-modal" classNameName="modal-toggle" />
+      <div classNameName="modal modal-bottom sm:modal-middle">
+        <div classNameName="modal-box">
           <label
             for="booking-modal"
-            class="btn btn-sm btn-circle absolute right-2 top-2"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
           >
             ✕
           </label>
-          <h3 className="font-bold text-lg text-secondary">
+          <h3 classNameName="font-bold text-lg text-secondary">
             Booking For: {name}
           </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label for="booking-modal" className="btn">
-              Yay!
-            </label>
-          </div>
+
+          <form
+            onSubmit={handleBooking}
+            className="grid grid-cols-1 gap-3 justify-items-center mt-2"
+          >
+            <input
+              type="text"
+              disabled
+              value={format(date, 'PP')}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <select
+              name="slot"
+              className="select select-bordered w-full max-w-xs"
+            >
+              {slots.map(slot => (
+                <option key={slot} value={slot}>
+                  {slot}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="phone"
+              placeholder="Phone Number"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="submit"
+              value="Submit"
+              className="btn primaryBtn w-full max-w-xs"
+            />
+          </form>
+          <div classNameName="modal-action"></div>
         </div>
       </div>
     </div>
