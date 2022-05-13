faf6540d6eef58cf3603fd9a72a9cc1b71cab3e0
progmamun <mamun333089@gmail.com>
2022-05-13 23:10:59 +0600

    booking-object-save

--- Client/src/Pages/Appointment/BookingModal.jsx
+++ Client/src/Pages/Appointment/BookingModal.jsx
@@ -6,11 +6,22 @@ import auth from '../../firebase.init';
 const BookingModal = ({ date, treatment, setTreatment }) => {
   const { _id, name, slots } = treatment;
   const [user, loading, error] = useAuthState(auth);
+  const formattedDate = format(date, 'PP');
 
   const handleBooking = event => {
     event.preventDefault();
     const slot = event.target.slot.value;
     console.log(_id, name, slot);
+
+    const booking = {
+      treatmentId: _id,
+      treatment: name,
+      date: formattedDate,
+      slot,
+      patient: user.displayName,
+      patientId: user.displayName,
+      phone: event.target.phone.value,
+    };
 
     // to close the modal
     setTreatment(null);
