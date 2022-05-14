85577e3576e8273f4282b5863f5b3ad3190303b5
progmamun <mamun333089@gmail.com>
2022-05-14 16:55:27 +0600

    update-api

--- Database/index.js
+++ Database/index.js
@@ -64,6 +64,13 @@ async function run() {
       res.send(services);
     });
 
+    app.get('/booking', async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post('/booking', async (req, res) => {
       const booking = req.body;
       const query = {
