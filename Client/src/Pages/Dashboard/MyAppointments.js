2ebc5b33d816c9e794fb47fb20446a5b7ac4cdc6
progmamun <mamun333089@gmail.com>
2022-05-15 15:21:21 +0600

    jwt-unauthorized-access

--- Client/src/Pages/Dashboard/MyAppointments.js
+++ Client/src/Pages/Dashboard/MyAppointments.js
@@ -3,49 +3,56 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 const MyAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
 
-    const [appointments, setAppointments] = useState([]);
-    const [user] = useAuthState(auth);
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      })
+        .then(res => {
+          console.log('res', res);
+          if (res.status === 401 || res.status === 403) {
+          }
+          return res.json();
+        })
+        .then(data => setAppointments(data));
+    }
+  }, [user]);
 
-    useEffect(() => {
-        if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`)
-                .then(res => res.json())
-                .then(data => setAppointments(data));
-        }
-    }, [user])
-
-    return (
-        <div>
-            <h2>My Appointments: {appointments.length}</h2>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Treatment</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            appointments.map((a, index) =><tr>
-                                <th>{index + 1}</th>
-                                <td>{a.patientName}</td>
-                                <td>{a.date}</td>
-                                <td>{a.slot}</td>
-                                <td>{a.treatment}</td>
-                            </tr>)
-                        }
-                        
-                        
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2>My Appointments: {appointments.length}</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((a, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{a.patientName}</td>
+                <td>{a.date}</td>
+                <td>{a.slot}</td>
+                <td>{a.treatment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
