525fe3177eba94a3f8030a13d1b9369f75c421bb
progmamun <mamun333089@gmail.com>
2022-05-20 08:17:24 +0600

    stipe-setup

---
+++ Client/src/Pages/Dashboard/CheckoutForm.js
@@ -0,0 +1,45 @@
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import React from 'react';
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button
+        className="btn btn-success btn-sm mt-4"
+        type="submit"
+        disabled={!stripe}
+      >
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
--- Client/src/Pages/Dashboard/Payment.js
+++ Client/src/Pages/Dashboard/Payment.js
@@ -1,9 +1,55 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe(
+  'pk_test_51IzPvXHcty6Ol617LagyBP9HJHVLDVzYCb8frJ7EzFZyZp03Dpsz1d1gt7Z9aXFEzkSClvF0zLsr5Ebr3SESAiYl00ETX6BtKf'
+);
 
 const Payment = () => {
+  const { id } = useParams();
+  const url = `http://localhost:5000/booking/${id}`;
+
+  const { data: appointment, isLoading } = useQuery(['booking', id], () =>
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    }).then(res => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div>
-      <h3>payment</h3>
+      <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+        <div class="card-body">
+          <p className="text-success font-bold">
+            Hello, {appointment.patientName}
+          </p>
+          <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+          <p>
+            Your Appointment:{' '}
+            <span className="text-orange-700">{appointment.date}</span> at{' '}
+            {appointment.slot}
+          </p>
+          <p>Please pay: ${appointment.price}</p>
+        </div>
+      </div>
+      <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm appointment={appointment} />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 };
